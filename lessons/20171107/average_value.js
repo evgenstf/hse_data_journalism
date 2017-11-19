0a8f9f47b3130f49e2789a574aae096e563c775a
Evgenii Kazakov <evgenstf@gmail.com>
2017-11-19 20:48:03 +0300

    Add average_value function

---
+++ lessons/20171107/average_value.js
@@ -0,0 +1,7 @@
+var averageValue = function(arr) {
+  var sum = 0;
+  for (let i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}                                                            
